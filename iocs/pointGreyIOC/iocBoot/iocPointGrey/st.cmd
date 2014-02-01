5bdafb5946dc01cc668b5b13fa46b7e5da499e80
Mark Rivers <rivers@cars.uchicago.edu>
2014-01-31 18:41:59 -0600

    Changes for Point Grey detector

--- iocs/pointGreyIOC/iocBoot/iocPointGrey/st.cmd
+++ iocs/pointGreyIOC/iocBoot/iocPointGrey/st.cmd
@@ -1,45 +1,33 @@
 < envPaths
 errlogInit(20000)
 
-dbLoadDatabase("$(TOP)/dbd/pixiradApp.dbd")
-pixiradApp_registerRecordDeviceDriver(pdbbase) 
+dbLoadDatabase("$(TOP)/dbd/pointGreyApp.dbd")
+pointGreyApp_registerRecordDeviceDriver(pdbbase) 
 
-epicsEnvSet("PREFIX", "13PR1:")
-epicsEnvSet("COMMAND_PORT", "PIXI_CMD")
-epicsEnvSet("STATUS_PORT", "2224")
-epicsEnvSet("DATA_PORT", "2223")
-epicsEnvSet("DATA_PORT_BUFFERS", "1500")
-epicsEnvSet("PORT",   "PIXI")
+epicsEnvSet("PREFIX", "13PG1:")
+epicsEnvSet("CAMERA_ID", "0")
+epicsEnvSet("PORT",   "PG1")
 epicsEnvSet("QSIZE",  "20")
-epicsEnvSet("XSIZE",  "476")
-epicsEnvSet("YSIZE",  "512")
+epicsEnvSet("XSIZE",  "648")
+epicsEnvSet("YSIZE",  "488")
 epicsEnvSet("NCHANS", "2048")
 
-###
-# Create the asyn port to talk to the Pixirad box on port 2222.
-drvAsynIPPortConfigure("$(COMMAND_PORT)","192.168.0.1:2222 HTTP", 0, 0, 0)
-asynOctetSetOutputEos($(COMMAND_PORT), 0, "\n")
-asynSetTraceIOMask($(COMMAND_PORT), 0, 2)
-#asynSetTraceMask($(COMMAND_PORT), 0, 9)
-
-pixiradConfig("$(PORT)", "$(COMMAND_PORT)", "$(DATA_PORT)", "$(STATUS_PORT)", $(DATA_PORT_BUFFERS), $(XSIZE), $(YSIZE))
+pointGreyConfig("$(PORT)", $(CAMERA_ID))
 asynSetTraceIOMask($(PORT), 0, 2)
 #asynSetTraceMask($(PORT), 0, 255)
 
 dbLoadRecords("$(ADCORE)/db/ADBase.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 dbLoadRecords("$(ADCORE)/db/NDFile.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
-dbLoadRecords("$(ADPIXIRAD)/db/pixirad.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1,SERVER_PORT=$(COMMAND_PORT)")
+dbLoadRecords("$(ADPOINTGREY)/db/pointGrey.template","P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
 
 # Create a standard arrays plugin
 NDStdArraysConfigure("Image1", 5, 0, "$(PORT)", 0, 0)
 dbLoadRecords("$(ADCORE)/db/NDPluginBase.template","P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,NDARRAY_PORT=$(PORT),NDARRAY_ADDR=0")
-dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=243712")
+dbLoadRecords("$(ADCORE)/db/NDStdArrays.template", "P=$(PREFIX),R=image1:,PORT=Image1,ADDR=0,TIMEOUT=1,TYPE=Int16,FTVL=SHORT,NELEMENTS=316224")
 
 # Load all other plugins using commonPlugins.cmd
 < $(ADCORE)/iocBoot/commonPlugins.cmd
-set_requestfile_path("$(ADPIXIRAD)/pixiradApp/Db")
-
-
+set_requestfile_path("$(ADPOINTGREY)/pointGreyApp/Db")
 
 iocInit()
 

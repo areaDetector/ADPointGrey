56926c20a011e87fd8ca006082dd47ac737a8c53
Mark Rivers <rivers@cars.uchicago.edu>
2014-08-18 17:19:26 -0500

    Make specific to GigE camera

--- iocs/pointGreyIOC/pointGreyApp/src/PGTest1.cpp
+++ iocs/pointGreyIOC/pointGreyApp/src/PGTest1.cpp
@@ -1,13 +1,13 @@
 /*
  * PGTest.cpp
  *
- * Test program to try to track down problem on Linux where areaDetector driver appears to set parameters
- * in camera that cause it to fail the next time it is run.
+ * Test program to try to track down problem on Linux which displays flaky behavior.
+ * Running this program under the GNU debugger causes a register write error
  *
  * Author: Mark Rivers
  *         Univerity of Chicago
  *
- * Created: April 20, 2014
+ * Created: May 21, 2014
  *
  */
 
@@ -39,13 +39,11 @@ public:
     int connectCamera();
 
     /* Data */
-    int cameraId_;
-    BusManager            *pBusMgr_;
-    PGRGuid               *pGuid_;
-    CameraBase            *pCameraBase_;
-    Camera                *pCamera_;
-    GigECamera            *pGigECamera_;
-    CameraInfo            *pCameraInfo_;
+    int          cameraId_;
+    BusManager   *pBusMgr_;
+    PGRGuid      *pGuid_;
+    CameraInfo   *pCameraInfo_;
+    GigECamera   *pGigECamera_;
 };
 
 /** Constructor for the PGTest class
@@ -54,9 +52,9 @@ PGTest::PGTest(int cameraId)
     : cameraId_(cameraId)
 {
     // Create camera control objects
-    pBusMgr_            = new BusManager;
-    pGuid_              = new PGRGuid;
-    pCameraInfo_        = new CameraInfo;
+    pBusMgr_        = new BusManager;
+    pGuid_          = new PGRGuid;
+    pCameraInfo_    = new CameraInfo;
 
     connectCamera();
 }
@@ -107,50 +105,47 @@ int PGTest::connectCamera(void)
     printf("%s::%s called BusManager::GetInterfaceTypeFromGuid, pBusMgr_=%p, interfaceType=%d\n",
         driverName, functionName, pBusMgr_, interfaceType);
     
-    // Create appropriate camera object
+    // Create camera object
     if (interfaceType == INTERFACE_GIGE) {
-        pCameraBase_ = new GigECamera;
-        pCamera_ = NULL;
-        pGigECamera_ = dynamic_cast<GigECamera*>(pCameraBase_);
-        if (pGigECamera_ == NULL) {
-            printf("%s::%s error casting camera to GigECamera\n",
-                driverName, functionName);
-            return PGError;
-        }
+        pGigECamera_ = new GigECamera;
     } else {
-        pCameraBase_ = new Camera;
-        pGigECamera_ = NULL;
-        pCamera_ = dynamic_cast<Camera*>(pCameraBase_);
-        if (pCamera_ == NULL) {
-            printf("%s::%s error casting camera to Camera\n",
-                driverName, functionName);
-            return PGError;
-        }   
+        printf("%s::%s: camera is not GigE camera\n",
+            driverName, functionName);
+        return PGError;   
     }
 
     // Connect to camera
-    printf("%s::%s calling CameraBase::Connect, pGuid_=%p\n",
+    printf("%s::%s calling GigECamera::Connect, pGuid_=%p\n",
         driverName, functionName, pGuid_);
-    error = pCameraBase_->Connect(pGuid_);
+    error = pGigECamera_->Connect(pGuid_);
     if (checkError(error, functionName, "Connect")) return PGError;
+    
+    // Disable heartbeat timeout
+    GigEProperty heartbeat;
+    heartbeat.propType = HEARTBEAT;
+    heartbeat.value = 0;
+    error = pGigECamera_->SetGigEProperty(&heartbeat);
+    if (checkError(error, functionName, "SetGigEProperty")) return PGError;
+    printf("%s::%s called GigECamera::SetGigEProperty(HEARTBEAT)\n",
+        driverName, functionName);
 
     // Get the camera information
-    error = pCameraBase_->GetCameraInfo(pCameraInfo_);
+    error = pGigECamera_->GetCameraInfo(pCameraInfo_);
     if (checkError(error, functionName, "GetCameraInfo")) return PGError;
-    printf("%s::%s called CameraBase::GetCameraInfo, pCameraInfo_=%p, pCameraInfo_->serialNumber=%d\n",
+    printf("%s::%s called GigECamera::GetCameraInfo, pCameraInfo_=%p, pCameraInfo_->serialNumber=%d\n",
         driverName, functionName, pCameraInfo_, pCameraInfo_->serialNumber);
     
     // Get and set the embedded image info
-    printf("%s::%s calling CameraBase::GetEmbeddedImageInfo, &embeddedInfo=%p\n",
+    printf("%s::%s calling GigECamera::GetEmbeddedImageInfo, &embeddedInfo=%p\n",
         driverName, functionName, &embeddedInfo);
-    error = pCameraBase_->GetEmbeddedImageInfo(&embeddedInfo);
+    error = pGigECamera_->GetEmbeddedImageInfo(&embeddedInfo);
     if (checkError(error, functionName, "GetEmbeddedImageInfo")) return PGError;
     // Force the timestamp and frame counter information to be on
     embeddedInfo.timestamp.onOff = true;
     embeddedInfo.frameCounter.onOff = true;
-    printf("%s::%s calling CameraBase::SetEmbeddedImageInfo, &embeddedInfo=%p\n",
+    printf("%s::%s calling GigECamera::SetEmbeddedImageInfo, &embeddedInfo=%p\n",
         driverName, functionName, &embeddedInfo);
-    error = pCameraBase_->SetEmbeddedImageInfo(&embeddedInfo);
+    error = pGigECamera_->SetEmbeddedImageInfo(&embeddedInfo);
     if (checkError(error, functionName, "SetEmbeddedImageInfo")) return PGError;
     
     return PGSuccess;
@@ -164,6 +159,12 @@ int main(int argc, char **argv)
         return -1;
     }
     int cameraId = atoi(argv[1]);
-    new PGTest(cameraId);
+    Error error;
+    PGTest *pPGTest1 = new PGTest(cameraId);
+    printf("%s::%s calling GigECamera::Disconnect\n",
+        driverName, "main");
+    error = pPGTest1->pGigECamera_->Disconnect();
+    if (pPGTest1->checkError(error, "main", "Disconnect")) return -1;
+    return 0;
 }
 

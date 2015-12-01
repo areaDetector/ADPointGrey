96b481d1b02a2fa741e1c5238256b4acdf52ea2d
Mark Rivers <rivers@cars.uchicago.edu>
2015-12-01 10:03:00 -0600

    Changed camera; added epicsThreadSleep calls to allow callbacks to complete; added prototype comment for pointGreyConfig

--- iocs/pointGreyIOC/iocBoot/iocPointGrey/st.cmd
+++ iocs/pointGreyIOC/iocBoot/iocPointGrey/st.cmd
@@ -12,7 +12,11 @@ epicsEnvSet("PREFIX", "13PG1:")
 #epicsEnvSet("CAMERA_ID", "9211601")
 # Use this line for a specific camera by serial number, in this case a Grasshopper3 USB-3.0 camera
 # This is the GSECARS tomography camera
-#epicsEnvSet("CAMERA_ID", "13510305")
+epicsEnvSet("CAMERA_ID", "13510305")
+# This is another GSECARS tomography camera
+#epicsEnvSet("CAMERA_ID", "15355695")
+# This is another GSECARS tomography camera
+#epicsEnvSet("CAMERA_ID", "15337483")
 # This is the 13-ID-D Grasshopper3 GigE camera, gse-pointgrey2
 #epicsEnvSet("CAMERA_ID", "14481221")
 # This is the 13-ID-D Grasshopper3 GigE camera, gse-pointgrey3
@@ -24,7 +28,7 @@ epicsEnvSet("PREFIX", "13PG1:")
 # This is the 2-BM GS3-U3-91S6M-C camera
 #epicsEnvSet("CAMERA_ID", "13510309")
 # Use this line for a specific camera by serial number, in this case a BlackFly GigE camera
-epicsEnvSet("CAMERA_ID", "13481965")
+#epicsEnvSet("CAMERA_ID", "13481965")
 # Use this line for a specific camera by serial number, in this case a Flea3 GigE camera
 # epicsEnvSet("CAMERA_ID", "14273040")
 # The port name for the detector
@@ -44,9 +48,12 @@ epicsEnvSet("EPICS_DB_INCLUDE_PATH", "$(ADCORE)/db")
 # Define NELEMENTS to be enough for a 2048x2048x3 (color) image
 epicsEnvSet("NELEMENTS", "12592912")
 
-pointGreyConfig("$(PORT)", $(CAMERA_ID), 0x0, 1)
+# pointGreyConfig(const char *portName, int cameraId, int traceMask, int memoryChannel,
+#                 int maxBuffers, size_t maxMemory, int priority, int stackSize)
+pointGreyConfig("$(PORT)", $(CAMERA_ID), 0x1, 0)
 asynSetTraceIOMask($(PORT), 0, 2)
-#asynSetTraceMask($(PORT), 0, 0x29)
+#asynSetTraceMask($(PORT), 0, 0xFF)
+#asynSetTraceFile($(PORT), 0, "asynTrace.out")
 #asynSetTraceInfoMask($(PORT), 0, 0xf)
 
 dbLoadRecords("$(ADPOINTGREY)/db/pointGrey.template", "P=$(PREFIX),R=cam1:,PORT=$(PORT),ADDR=0,TIMEOUT=1")
@@ -68,10 +75,19 @@ iocInit()
 # save things every thirty seconds
 create_monitor_set("auto_settings.req", 30,"P=$(PREFIX)")
 
+# Wait for enum callbacks to complete
+epicsThreadSleep(1.0)
 
-# There is a problem with some records for which PINI=YES does not work because of timing or ordering
-# For those records to process after iocInit
-
+# Records with dynamic enums need to be processed again because the enum values are not available during iocInit.  
+dbpf("$(PREFIX)cam1:Format7Mode.PROC", "1")
 dbpf("$(PREFIX)cam1:PixelFormat.PROC", "1")
+
+# Wait for callbacks on the property limits (DRVL, DRVH) to complete
+epicsThreadSleep(1.0)
+
+# Records that depend on the state of the dynamic enum records or property limits also need to be processed again
+# Other property records may need to be added to this list
 dbpf("$(PREFIX)cam1:FrameRate.PROC", "1")
 dbpf("$(PREFIX)cam1:FrameRateValAbs.PROC", "1")
+dbpf("$(PREFIX)cam1:AcquireTime.PROC", "1")
+
